812d65f5a9d8e57e236cd3fddff437287bbdd710
Iikka Borgström <iikka.borgstrom@duodecim.fi>
2020-01-03 16:01:17 +0200

    initial commit

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <view class="container">
+    <Statusbar color="#459fed" />
+    <Header title="Helsinki App" />  
+  </view>
+</template>
+
+<script>
+import Statusbar from './components/Statusbar'
+import Header from './components/Header'
+
+export default {
+  components: {
+    Statusbar,
+    Header
+  }
+}
+</script>
+
+<style>
+
+.container {
+    flex: 1;
+    background-color: #fff;
+    
+}
+</style>
+
---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <view class="header">
+        <text class="title">{{ title }}</text>
+    </view>
+</template>
+
+<script>
+export default {
+    props: ['title']
+}
+</script>
+
+<style>
+.header {
+    height: 60px;
+    background-color:#0000bf;
+    justify-content: center;
+    align-items: center;
+}
+.title {
+    color: #F3F3F3;
+    font-size: 28px;
+    font-weight: 900;
+    text-transform: lowercase;
+}
+</style>
